901f17ce88eac4ffecfa3ca971605556643930c1
Bartel-Jaap1234 <l.kruize@st.hanze.nl>
2017-06-27 18:22:00 +0000

    nagios werkt

--- scriptnagios.sh
+++ scriptnagios.sh
@@ -21,22 +21,23 @@ sudo make install-init
 sudo make install-config
 sudo /usr/bin/install -c -m 644 sample-config/httpd.conf /etc/apache2/sites-available/nagios.conf
 
-#cd ~
-#curl -L -O http://nagios-plugins.org/download/nagios-plugins-2.2.1.tar.gz
-#tar xvf nagios-plugins-*.tar.gz
+cd ~
+curl -L -O http://nagios-plugins.org/download/nagios-plugins-2.2.1.tar.gz
+tar xvf nagios-plugins-*.tar.gz
 
-#cd nagios-plugins-*
-#./configure --with-nagios-user=nagios --with-nagios-group=nagios --with-openssl
-#make
-#sudo make install
+cd nagios-plugins-*
+./configure --with-nagios-user=nagios --with-nagios-group=nagios --with-openssl
+make
+sudo make install
 
-#cd ~
-#curl -L -O http://downloads.sourceforge.net/project/nagios/nrpe-2.x/nrpe-2.15/nrpe-2.15.tar.gz
-#tar xvf nrpe-*.tar.gz
+cd ~
+curl -L -O http://downloads.sourceforge.net/project/nagios/nrpe-2.x/nrpe-2.15/nrpe-2.15.tar.gz
+tar xvf nrpe-*.tar.gz
 
-#cd nrpe-*
-#./configure --enable-command-args --with-nagios-user=nagios --with-nagios-group=nagios --with-ssl=/usr/bin/openssl --with-ssl-lib=/usr/lib/x86_64-linux-gnu
-#make all
-#sudo make install
-#sudo make install-xinetd
-#sudo make install-daemon-config
+cd nrpe-*
+./configure --enable-command-args --with-nagios-user=nagios --with-nagios-group=nagios --with-ssl=/usr/bin/openssl --with-ssl-lib=/usr/lib/x86_64-linux-gnu
+make all
+sudo make install
+sudo make install-xinetd
+sudo make install-daemon-config
+
